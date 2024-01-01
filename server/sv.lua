1c21e281eecbd3d97e7b8facf2b1d185dd5aedd7
Whereiam <84282589+WhereiamL@users.noreply.github.com>
2024-01-01 21:38:16 +0100

    Update sv.lua
     Better code

--- server/sv.lua
+++ server/sv.lua
@@ -8,72 +8,68 @@ function SendDiscordWebhookMessage(source, wash, percent)
             {
                 title = "Washing machine logs",
                 description = string.format("Player ID: %s\nMoney Washed: %s\nReceived: %s", source, wash, percent),
-                color = 65280, -- Optional: Color for the embed in decimal format
-                timestamp = os.date('!%Y-%m-%dT%H:%M:%SZ') -- Optional: Timestamp in ISO 8601 format
+                color = 65280,
+                timestamp = os.date('!%Y-%m-%dT%H:%M:%SZ')
             }
         }
     }
     PerformHttpRequest(DISCORD_WEBHOOK_URL, function(statusCode, responseData, headers)
-        -- Handle the response here (optional)
-        print("Discord webhook message sent:", statusCode)
-        print(responseData)
-    end, 'POST', json.encode(data), { ['Content-Type'] = 'application/json' })
+        if statusCode == 200 then
+            print("Discord webhook message sent successfully")
+        else
+            print("Failed to send Discord webhook message:", statusCode)
+            print(responseData)
+        end
+    end, 'POST', json.encode(data), { ['Content-Type'] = 'application/JSON' })
 end
+
 RegisterNetEvent('sqc:server:check:job')
 AddEventHandler('sqc:server:check:job', function()
- local source = source
- local xPlayer = ESX.GetPlayerFromId(source)
- local hasJob = false
+    local source = source
+    local xPlayer = ESX.GetPlayerFromId(source)
 
-if Config.jobRestriction then
- for _ , v in ipairs(Config.whiteListJobs) do
-   if xPlayer.getJob().name == v then
-    TriggerClientEvent('sqc:client:allowed:job', source)
-    hasJob = true
-    return
-   end
-end
-end
-if not Config.jobRestriction then
-    TriggerClientEvent('sqc:client:allowed:job', source)
-    return
-end
-if not hasJob then
-    TriggerClientEvent('ox_lib:notify', source, {
-        title = 'Washing machine',
-        description = 'You dont have the required job to do this.',
-        duration = 5000,
-        type = 'error',
-        icon = 'fa-solid fa-money-bill-1-wave'
-    })
-end
+    if not Config.jobRestriction or IsJobWhitelisted(xPlayer.getJob().name) then
+        TriggerClientEvent('sqc:client:allowed:job', source)
+    else
+        TriggerClientEvent('ox_lib:notify', source, {
+            title = 'Washing machine',
+            description = 'You do not have the required job to do this.',
+            duration = 5000,
+            type = 'error',
+            icon = 'fa-solid fa-money-bill-1-wave'
+        })
+    end
 end)
+
+function IsJobWhitelisted(jobName)
+    for _, v in ipairs(Config.whiteListJobs) do
+        if jobName == v then
+            return true
+        end
+    end
+    return false
+end
+
 RegisterNetEvent('sqc:server:washingMoney')
 AddEventHandler('sqc:server:washingMoney', function(chance, hasMoney, id, distance, playerPos, wash)
-    if chance      == nil then return end
-    if hasMoney    == nil then return end
-    if id          == nil then return end
-    if distance    == nil then return end
-    if playerPos   == nil then return end
-    if wash        == nil then return end
+    if not chance or not hasMoney or not id or not distance or not playerPos or not wash then
+        return
+    end
 
-    local source    = source
+    local source = source
     local sourcePed = GetPlayerPed(source)
     local sourcePos = GetEntityCoords(sourcePed)
     local countItem = ox_inventory:Search(source, 'count', 'black_money')
-    local percent   = wash * Config.percentage
+    local percent = wash * Config.percentage
 
-    if countItem <= 10 then
-        return
-    else
-        if ox_inventory:CanCarryItem(source, 'money', percent) then
-            ox_inventory:RemoveItem(source, 'black_money', wash)
-            ox_inventory:AddItem(source, 'money', percent)
-            SendDiscordWebhookMessage(source, wash, percent)
-        end
+    if countItem > 10 and ox_inventory:CanCarryItem(source, 'money', percent) then
+        ox_inventory:RemoveItem(source, 'black_money', wash)
+        ox_inventory:AddItem(source, 'money', percent)
+        SendDiscordWebhookMessage(source, wash, percent)
+
         TriggerClientEvent('ox_lib:notify', source, {
             title = 'Washing machine',
-            description = 'You have washed '..wash..' and received '..percent,
+            description = string.format('You have washed %s and received %s', wash, percent),
             duration = 4000,
             type = 'success',
             icon = 'fa-solid fa-money-bill-1-wave'
